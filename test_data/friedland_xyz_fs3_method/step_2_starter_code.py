5056c20f6f81886165625ba5f096ae2f20139fe1
Sabrina Tan <sabrinatan@Sabrinas-MacBook-Air.local>
2025-10-20 17:25:30 -0700

    need to navigate up more folders for step tests

--- test_data/friedland_xyz_fs3_method/step_2_starter_code.py
+++ test_data/friedland_xyz_fs3_method/step_2_starter_code.py
@@ -3,7 +3,7 @@ import numpy as np
 import chainladder as cl
 from pathlib import Path
 
-data_dir = Path(__file__).resolve().parent
+data_dir = Path(__file__).resolve().parent.parent.parent.parent / 'test_data' / 'friedland_xyz_fs3_method'
 reported_count_data_path = data_dir / 'reported_claim_count_triangle.csv'
 cwp_count_data_path = data_dir / 'closed_with_pay_claim_count_triangle.csv'
 
